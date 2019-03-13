0239d436482edef9e635d42c8548dd6fba88ffb7
Andre Guenther <reh@boldreh.com>
2019-03-13 14:26:57 +0100

    make the provide part available as part of this package

---
+++ dist/apiprovide.cjs.js
@@ -0,0 +1,37 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function provide(packageName, pack) {
+    // -- START -- static loader
+    var unresolvedPackages = {};
+    var providedPackages = {};
+    var loaderName = 'pssmasloader';
+    var loader = window[loaderName] = window[loaderName] || {
+        _: {
+            u: unresolvedPackages,
+            p: providedPackages,
+        },
+        require: function (packageName, cb) {
+            var pack = providedPackages[packageName];
+            if (pack !== undefined) {
+                cb(pack, null);
+            }
+            else {
+                unresolvedPackages[packageName] = unresolvedPackages[packageName] || [];
+                unresolvedPackages[packageName].push(cb);
+            }
+        },
+    };
+    unresolvedPackages = loader._.u;
+    providedPackages = loader._.p;
+    // -- END -- static loader
+    var unresolvedRequires = unresolvedPackages[packageName] || [];
+    providedPackages[packageName] = pack;
+    for (var i = 0; i < unresolvedRequires.length; i++) {
+        unresolvedRequires[i](pack, null);
+    }
+    return pack;
+}
+
+exports.provide = provide;
---
+++ test/apiprovide.test.ts
@@ -0,0 +1,97 @@
+
+import sinon from 'sinon';
+
+import {provide} from '../src/apiprovide'
+describe('apiprovide', () => {
+
+    it('puts provided api in the provided pile (own loader)', () => {
+        // given
+        const fakeApi = {} as any;
+        const window : any = (global as any).window = {}
+        
+        // when: providing the API
+        provide('whoareyou', fakeApi)
+
+        // then
+        expect(window.pssmasloader._.p['whoareyou']).toEqual(fakeApi);
+    })
+
+    it('puts provided api in the provided pile (3rd party loader)', () => {
+        // given
+        const fakeApi = {} as any;
+        const window : any = (global as any).window = {
+            pssmasloader: {
+                _: {
+                    p: {},
+                    u: {}
+                }
+            }
+        }
+        const providedPackagesPile = window.pssmasloader._.p;
+        
+        // when: providing the API
+        provide('whoareyou', fakeApi)
+
+        // then
+        expect(window.pssmasloader._.p['whoareyou']).toEqual(fakeApi);
+        expect(providedPackagesPile).toEqual(window.pssmasloader._.p);
+    })
+
+    it('calls up unresolved requires previous to providing', () => {
+        // given
+        const fakeApi = {} as any;
+        const whoareYouRequest = sinon.spy();
+        const whoareYouRequest2 = sinon.spy();
+        (global as any).window = {
+            pssmasloader: {
+                _: {
+                    p: {},
+                    u: {
+                        whoareyou: [whoareYouRequest, whoareYouRequest2]
+                    }
+                }
+            }
+        }
+        
+        // when: providing the API
+        provide('whoareyou', fakeApi)
+
+        // then
+        sinon.assert.calledWith(whoareYouRequest, fakeApi, null);
+        sinon.assert.calledWith(whoareYouRequest2, fakeApi, null);
+    })
+
+    it('installs its own loader that is able to queue unresolved packages', () => {
+        // given
+        const window : any = (global as any).window = {}
+        
+        // when: providing the API
+        provide('whoareyou', {})
+
+        // and when: 3rd party services uses the require functionality again
+        const fooCb = () => {}
+        window.pssmasloader.require('fooservice:v1', fooCb)
+
+        // then
+        expect(window.pssmasloader._.u['fooservice:v1']).toEqual([fooCb]);
+    })
+
+    it('installs its own loader that is able to load provided packages', () => {
+        // given
+        const fooApi = {};
+        const window : any = (global as any).window = {}
+        
+        // when: providing the API
+        provide('whoareyou', {});
+
+        // and when: service is registered
+        window.pssmasloader._.p['fooservice:v1'] = fooApi;
+
+        // and when: 3rd party services uses the require functionality again
+        const requireCb = sinon.spy();
+        window.pssmasloader.require('fooservice:v1', requireCb)
+
+        sinon.assert.calledWith(requireCb, fooApi, null)
+    })
+
+})
