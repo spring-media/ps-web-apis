f96d9efa3904e26db83b605d18a62861c8d35ba1
fgerardwltn24 <francois.gerard@spring-media.de>
2019-03-15 09:02:44 +0100

    fixed syntax

--- dist/ps-web-apis.d.ts
+++ dist/ps-web-apis.d.ts
@@ -1,4 +1,33 @@
 export interface WhoamiV1 {
-    isLoggedIn: () => boolean;
+    /**
+     * gives information if user is currently loggedin from ui perspective
+    */
+    isLoggedIn(): boolean;
+    /**
+     * fn will be called after the state changed from logged to logged out or vice versa
+     */
+    onStateChange(fn: () => void): void;
+    /**
+     * will assert valid not outdated session before fetch will be done. backend credentials will be added automatically
+     * an error is resolved if session is invalid and not refeshable (= user logged out)
+     */
+    authorizedFetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+    /**
+     * will assert valid not outdated session before fn is called
+     * an error is resolved if session is invalid and not refeshable (= user logged out)
+     */
+    ensureUserHasAuthorization(): Promise<void>;
+    /**
+     * will start login-process (e.g. go to sso-login)
+     */
+    doLogin(additonalParameter?: Map<String, String[]>): void;
+    /**
+    * will start registration-process (e.g. go to sso-register)
+    */
+    doRegister(additonalParameter?: Map<String, String[]>): void;
+    /**
+     * will start logout-process (e.g. go to sso-logout)
+     */
+    doLogout(additonalParameter?: Map<String, String[]>): void;
 }
 export declare function whoamiV1(): Promise<WhoamiV1>;
--- src/ps-web-apis.ts
+++ src/ps-web-apis.ts
@@ -44,16 +44,16 @@ export interface WhoamiV1 {
     /** 
      * gives information if user is currently loggedin from ui perspective
     */
-    isLoggedIn(): bool
+    isLoggedIn(): boolean
     /**
      * fn will be called after the state changed from logged to logged out or vice versa
      */
-    onStateChange( fn: (void -> void) )  
+    onStateChange( fn: () => void ): void  
     /**
      * will assert valid not outdated session before fetch will be done. backend credentials will be added automatically
      * an error is resolved if session is invalid and not refeshable (= user logged out)
      */
-    authorizedFetch(input: RequestInfo, init?: RequestInit) => Promise<Response>
+    authorizedFetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>
     /**
      * will assert valid not outdated session before fn is called
      * an error is resolved if session is invalid and not refeshable (= user logged out)
@@ -62,15 +62,15 @@ export interface WhoamiV1 {
     /**
      * will start login-process (e.g. go to sso-login)
      */
-    doLogin(additonalParameter?: Map<String, String[]): void
+    doLogin(additonalParameter?: Map<String, String[]>): void
      /**
      * will start registration-process (e.g. go to sso-register)
      */
-    doRegister(additonalParameter?: Map<String, String[]): void
+    doRegister(additonalParameter?: Map<String, String[]>): void
     /**
      * will start logout-process (e.g. go to sso-logout)
      */
-    doLogout(additonalParameter?: Map<String, String[]): void
+    doLogout(additonalParameter?: Map<String, String[]>): void
 }
 
 export function whoamiV1() : Promise<WhoamiV1> {
