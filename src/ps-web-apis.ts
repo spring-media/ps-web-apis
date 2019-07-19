0cc4f691c7d6e3f6e3a2c7d213ba2a9a4827402c
abujnick <abujnick@asideas.de>
2019-07-19 08:46:08 +0200

    remove doLogout and onStateChange

--- src/ps-web-apis.ts
+++ src/ps-web-apis.ts
@@ -46,10 +46,6 @@ export interface WhoamiV1 {
     */
     isLoggedIn(): boolean
     /**
-     * fn will be called after the state changed from logged to logged out or vice versa
-     */
-    onStateChange( fn: () => void ): void  
-    /**
      * will assert valid not outdated session before fetch will be done. backend credentials will be added automatically
      * an error is resolved if session is invalid and not refeshable (= user logged out)
      */
@@ -67,10 +63,6 @@ export interface WhoamiV1 {
      * will start registration-process (e.g. go to sso-register)
      */
     doRegister(additionalParameter?: Map<String, String[]>): void
-    /**
-     * will start logout-process (e.g. go to sso-logout)
-     */
-    doLogout(additionalParameter?: Map<String, String[]>): void
     /**
      * will update access token and therefore content entitlements to current state 
      */
