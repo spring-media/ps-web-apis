0b45da20363aa4e827a210b96db409f57b867c4e
Babette Wagner <babette.wagner@asideas.de>
2019-10-08 12:21:23 +0200

    SPR-4690: remove test steps (no ghostinspector tests present)

--- dist/ps-web-apis.esm.js
+++ dist/ps-web-apis.esm.js
@@ -2,10 +2,10 @@ function requireApi(name) {
     // -- START -- static loader
     var unresolvedPackages = {};
     var providedPackages = {};
-    var loaderName = 'pssmasloader';
+    var loaderName = "pssmasloader";
     // set or reuse existing loader implementation
-    var loader = window[loaderName] = window[loaderName] || {
-        // Requires packageName and returns it via callback 
+    var loader = (window[loaderName] = window[loaderName] || {
+        // Requires packageName and returns it via callback
         require: function (packageName, cb) {
             var pack = providedPackages[packageName];
             if (pack !== undefined) {
@@ -21,9 +21,9 @@ function requireApi(name) {
         // private state
         _: {
             u: unresolvedPackages,
-            p: providedPackages,
+            p: providedPackages
         }
-    };
+    });
     // -- END -- static loader
     return new Promise(function (resolve, reject) {
         loader.require(name, function (res, error) {
@@ -37,7 +37,7 @@ function requireApi(name) {
     });
 }
 function whoamiV1() {
-    return requireApi('whoami:v1');
+    return requireApi("whoami:v1");
 }
 
 export { whoamiV1 };
